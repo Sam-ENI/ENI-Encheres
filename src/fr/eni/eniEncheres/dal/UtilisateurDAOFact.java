91f1a559541e924444bdc56f9da292bcb6ca69f4
Samuel Fradin <samuel.fradin2021@campus-eni.fr>
2021-08-17 11:34:52 +0200

    modif dans la servlet CreationCompteServlet

--- src/fr/eni/eniEncheres/dal/UtilisateurDAOFact.java
+++ src/fr/eni/eniEncheres/dal/UtilisateurDAOFact.java
@@ -2,6 +2,6 @@ package fr.eni.eniEncheres.dal;
 
 public class UtilisateurDAOFact {
 	public static UtilisateurDAO getInstanceDAO() {
-		return new UtilisateurDAOMock();
+		return new UtilisateurDAOImpl();
 	}
 }
