b1f323dd98cb424419160d9607600cb01e432150
AntoineDevWeb <antoine.developweb@gmail.com>
2021-08-19 16:10:39 +0200

    Modification du nom de la classe ArticleVenduImpl en ArticleVenduDAOImpl

--- src/fr/eni/eniEncheres/dal/jdbc/ArticleVenduDAOImpl.java
+++ src/fr/eni/eniEncheres/dal/jdbc/ArticleVenduDAOImpl.java
@@ -15,7 +15,7 @@ import fr.eni.eniEncheres.bo.Utilisateur;
 import fr.eni.eniEncheres.dal.ArticleVenduDAO;
 import fr.eni.eniEncheres.dal.DALException;
 
-public class ArticleVenduImpl implements ArticleVenduDAO {
+public class ArticleVenduDAOImpl implements ArticleVenduDAO {
 	private final String INSERT = "INSERT INTO ARTICLES_VENDUS(nom_article,description, date_debut_encheres,date_fin_encheres, prix_initial,prix_vente,no_utilisateur,no_categorie) VALUES (?,?,?,?,?,?,?,?)";
 	private final String UPDATE = "UPDATE ARTICLES_VENDUS SET nom_article=?,description=?, date_debut_encheres=?,date_fin_encheres=?, prix_initial=?,prix_vente=?,no_utilisateur=?,no_categorie=? WHERE no_article = ?";
 	private final String DELETE = "DELETE FROM ARTICLES_VENDUS WHERE no_article =?";
