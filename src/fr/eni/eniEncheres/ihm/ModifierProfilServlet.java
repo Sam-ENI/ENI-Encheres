8770c8f40260d934fb688ab9ad6e62c09c6d280d
Valentin Brault <valentin.brault0@gmail.com>
2021-08-19 08:59:22 +0200

    - Ajout : Bouton Supprimer le compte -> modifierprofil.jsp

--- src/fr/eni/eniEncheres/ihm/ModifierProfilServlet.java
+++ src/fr/eni/eniEncheres/ihm/ModifierProfilServlet.java
@@ -7,13 +7,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.eniEncheres.bll.BLLException;
+import fr.eni.eniEncheres.bll.UtilisateurManager;
+import fr.eni.eniEncheres.bll.UtilisateurManagerFactory;
+
 /**
  * Servlet implementation class ModifierProfilServlet
  */
 @WebServlet("/ModifierProfilServlet")
 public class ModifierProfilServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private UtilisateurManager manager = UtilisateurManagerFactory.getInstance();
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -34,8 +39,25 @@ public class ModifierProfilServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		boolean hidden =  true;
+		UtilisateurModel model = (UtilisateurModel) request.getSession().getAttribute("model");
+		
+		
+		
+		// BOUTON SUPPRIMER LE COMPTE
+		if (request.getParameter("supprimerCompe")!= null) {
+			try {
+				manager.deleteUtilisateur(model.getUtilisateur());
+				hidden = false;
+				model = null;
+			} catch (BLLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		request.getSession().setAttribute("hidden", hidden);
+		request.getSession().setAttribute("model", model);
 	}
 
 }
